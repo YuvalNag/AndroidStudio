d05157f6f283260784d45f6acf36eee38306acad
nissy34 <33037628+nissy34@users.noreply.github.com>
2017-11-01 23:16:52 +0200

    as

--- Targil2_Android/Adapter/app/src/main/java/com/yn/user/adapter/MainActivity.java
+++ Targil2_Android/Adapter/app/src/main/java/com/yn/user/adapter/MainActivity.java
@@ -3,6 +3,7 @@ package com.yn.user.adapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Toast.makeText(this, "create", Toast.LENGTH_LONG).show();
         List<String> text = new ArrayList<String>();
         for (int i =0;i<50;i++)
             text.add("This is the Button " + (i+1) );
