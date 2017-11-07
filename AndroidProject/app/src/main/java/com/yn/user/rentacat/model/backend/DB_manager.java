b1b883405df4860160e1d384edb5c057255dd0a6
YuvalNag <33052299+YuvalNag@users.noreply.github.com>
2017-11-07 15:53:36 +0200

    beckend

---
+++ AndroidProject/app/src/main/java/com/yn/user/rentacat/model/backend/DB_manager.java
@@ -0,0 +1,8 @@
+package com.yn.user.rentacat.model.backend;
+
+/**
+ * Created by USER on 07/11/2017.
+ */
+
+public interface DB_manager {
+}
---
+++ AndroidProject/app/src/main/java/com/yn/user/rentacat/model/datasource/List_DB.java
@@ -0,0 +1,8 @@
+package com.yn.user.rentacat.model.datasource;
+
+/**
+ * Created by USER on 07/11/2017.
+ */
+
+public class List_DB {
+}
