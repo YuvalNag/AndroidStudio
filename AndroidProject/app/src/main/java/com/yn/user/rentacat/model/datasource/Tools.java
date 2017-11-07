85890d309828869828abb15a8404fc54a398da18
YuvalNag <33052299+YuvalNag@users.noreply.github.com>
2017-11-07 20:27:52 +0200

    yuval_Tool

--- AndroidProject/app/src/main/java/com/yn/user/rentacat/model/datasource/Tools.java
+++ AndroidProject/app/src/main/java/com/yn/user/rentacat/model/datasource/Tools.java
@@ -2,6 +2,9 @@ package com.yn.user.rentacat.model.datasource;
 
 import android.content.ContentValues;
 
+import com.yn.user.rentacat.model.entities.Client;
+import com.yn.user.rentacat.model.entities.Order;
+
 /**
  * Created by nissy34 on 07/11/2017.
  */
@@ -17,4 +20,22 @@ public class Tools {
 
         return contentValues;
     }
-}
+
+
+
+
+
+
+
+    public static ContentValues ClientToContentValues(Client client) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(AcademyContract.Lecturer.ID, lecturer.getId());
+        contentValues.put(AcademyContract.Lecturer.NAME, lecturer.getName());
+        contentValues.put(AcademyContract.Lecturer.PHONE, lecturer.getPhone());
+        contentValues.put(AcademyContract.Lecturer.SENIORITY, lecturer.getSeniority());
+        return contentValues;
+    }
+    public static ContentValues OrderToContentValues(Order order) {}
+
+
+    }
