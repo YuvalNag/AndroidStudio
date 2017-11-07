4e3c42bd1549c23ea1b5d1445dbf25efb096fdf8
nissy34 <33037628+nissy34@users.noreply.github.com>
2017-11-07 20:28:39 +0200

    nissy_tool

--- AndroidProject/app/src/main/java/com/yn/user/rentacat/model/datasource/Tools.java
+++ AndroidProject/app/src/main/java/com/yn/user/rentacat/model/datasource/Tools.java
@@ -2,12 +2,18 @@ package com.yn.user.rentacat.model.datasource;
 
 import android.content.ContentValues;
 
+import com.yn.user.rentacat.model.entities.Address;
+import com.yn.user.rentacat.model.entities.Branch;
+import com.yn.user.rentacat.model.entities.Car;
+import com.yn.user.rentacat.model.entities.CarModel;
+import com.yn.user.rentacat.model.entities.Client;
+
 /**
  * Created by nissy34 on 07/11/2017.
  */
 
 public class Tools {
-    public static ContentValues LecturerToContentValues(Lecturer lecturer) {
+    public static ContentValues AddressToContentValues(Address lecturer) {
 
         ContentValues contentValues = new ContentValues();
         contentValues.put(AcademyContract.Lecturer.ID, lecturer.getId());
@@ -17,4 +23,16 @@ public class Tools {
 
         return contentValues;
     }
+    public static ContentValues BranchToContentValues(Branch branch) {}
+    public static ContentValues CarToContentValues(Car car) {}
+    public static ContentValues CarModelToContentValues(Branch branch) {}
+
+
+
+    public static Address ContentValuesToAddress(ContentValues contentValues) {}
+    public static Branch ContentValuesToBranch(ContentValues contentValues) {}
+    public static Car ContentValuesToCar(ContentValues contentValues) {}
+    public static CarModel ContentValuesToCarModel(ContentValues contentValues) {}
+    public static Client ContentValuesToClient(ContentValues contentValues) {}
+
 }
