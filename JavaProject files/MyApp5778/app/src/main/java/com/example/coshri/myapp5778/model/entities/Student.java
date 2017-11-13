c22b666c5925c916da70d747580905fa1004916f
nissy34 <33037628+nissy34@users.noreply.github.com>
2017-11-13 17:19:15 +0200

    other example

---
+++ JavaProject files/MyApp5778/app/src/main/java/com/example/coshri/myapp5778/model/entities/Student.java
@@ -0,0 +1,23 @@
+package com.example.coshri.myapp5778.model.entities;
+import java.util.Date;
+
+/**
+ * Created by mailo on 26/11/2016.
+ */
+
+    public class Student extends  Person{
+
+
+
+    private Year year;
+
+    public Year getYear() {
+        return year;
+    }
+
+    public void setYear(Year year) {
+        this.year = year;
+    }
+
+
+}
