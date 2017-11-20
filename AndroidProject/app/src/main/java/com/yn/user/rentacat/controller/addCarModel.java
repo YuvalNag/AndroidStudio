5abf2647ac786e6093f4957bc14f8e03f6f54a14
nissy34 <33037628+nissy34@users.noreply.github.com>
2017-11-20 19:18:58 +0200

    sss

--- AndroidProject/app/src/main/java/com/yn/user/rentacat/controller/addCarModel.java
+++ AndroidProject/app/src/main/java/com/yn/user/rentacat/controller/addCarModel.java
@@ -89,7 +89,7 @@ public class addCarModel extends AppCompatActivity {
 
                 Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
 
-                ((ImageButton) findViewById(R.id.model_image)).setImageBitmap(scaleDown(bitmap,500,true));
+                ((ImageButton) findViewById(R.id.model_image)).setImageBitmap(Tools.scaleDown(bitmap,500,true));
 
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
@@ -97,18 +97,7 @@ public class addCarModel extends AppCompatActivity {
         }
     }
 
-    public static Bitmap scaleDown(Bitmap realImage, float maxImageSize,
-                                   boolean filter) {
-        float ratio = Math.min(
-                (float) maxImageSize / realImage.getWidth(),
-                (float) maxImageSize / realImage.getHeight());
-        int width = Math.round((float) ratio * realImage.getWidth());
-        int height = Math.round((float) ratio * realImage.getHeight());
 
-        Bitmap newBitmap = Bitmap.createScaledBitmap(realImage, width,
-                height, filter);
-        return newBitmap;
-    }
 
 //TODO check permissions
 
