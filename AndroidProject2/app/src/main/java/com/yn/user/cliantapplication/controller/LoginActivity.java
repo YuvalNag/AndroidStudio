e448ae258ef109c57d951442991cdc3bdd4ddf32
YuvalNag <33052299+YuvalNag@users.noreply.github.com>
2017-12-28 20:01:06 +0200

    login

--- AndroidProject2/app/src/main/java/com/yn/user/cliantapplication/controller/LoginActivity.java
+++ AndroidProject2/app/src/main/java/com/yn/user/cliantapplication/controller/LoginActivity.java
@@ -23,7 +23,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import com.yn.user.cliantapplication.R;
 import com.yn.user.cliantapplication.model.backend.AppContract;
+import com.yn.user.cliantapplication.model.backend.DBManagerFactory;
 import com.yn.user.cliantapplication.model.backend.SHA_256_Helper;
+import com.yn.user.cliantapplication.model.entities.Client;
+
+import java.util.List;
 
 /**
  * A login screen that offers login via email/password.
@@ -44,7 +48,7 @@ public class LoginActivity extends AppCompatActivity  {
     };
 */
     private SharedPreferences mSharedPreferences;
-    private Cursor mCursor;
+    private Client mClient;
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
@@ -60,19 +64,10 @@ public class LoginActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        getManagersCursor();
         mSharedPreferences =  PreferenceManager.getDefaultSharedPreferences(this);
-        /*long lo=mSharedPreferences.getLong(getString(R.string.login_user_id),0);
-        if(mSharedPreferences.getBoolean(getString(R.string.is_login),false)){
-            Intent mainIntent =new Intent(LoginActivity.this,MainActivity.class);
-            mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(mainIntent);
-        }*/
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
        // populateAutoComplete();
-
         mPasswordView = (EditText) findViewById(R.id.password);
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
@@ -97,50 +92,7 @@ public class LoginActivity extends AppCompatActivity  {
         mProgressView = findViewById(R.id.login_progress);
     }
 
-   /* private void populateAutoComplete() {
-        if (!mayRequestContacts()) {
-            return;
-        }
 
-        getLoaderManager().initLoader(0, null, this);
-    }
-
-    private boolean mayRequestContacts() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            return true;
-        }
-        if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        }
-        if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
-            Snackbar.make(mEmailView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
-                    .setAction(android.R.string.ok, new View.OnClickListener() {
-                        @Override
-                        @TargetApi(Build.VERSION_CODES.M)
-                        public void onClick(View v) {
-                            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-                        }
-                    });
-        } else {
-            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-        }
-        return false;
-    }
-
-    *//**
-     * Callback received when a permissions request has been completed.
-     *//*
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_READ_CONTACTS) {
-            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                populateAutoComplete();
-            }
-        }
-    }
-
-*/
     /**
      * Attempts to sign in or register the account specified by the login form.
      * If there are form errors (invalid email, missing fields, etc.), the
@@ -249,60 +201,7 @@ public class LoginActivity extends AppCompatActivity  {
         }
     }
 
-   /* @Override
-    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        return new CursorLoader(this,
-                // Retrieve data rows for the device user's 'profile' contact.
-                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
-                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
 
-                // Select only email addresses.
-                ContactsContract.Contacts.Data.MIMETYPE +
-                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
-                .CONTENT_ITEM_TYPE},
-
-                // Show primary email addresses first. Note that there won't be
-                // a primary email address if the user hasn't specified one.
-                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-        List<String> emails = new ArrayList<>();
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast()) {
-            emails.add(cursor.getString(ProfileQuery.ADDRESS));
-            cursor.moveToNext();
-        }
-
-        addEmailsToAutoComplete(emails);
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> cursorLoader) {
-
-    }
-
-    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
-        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(LoginActivity.this,
-                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
-
-        mEmailView.setAdapter(adapter);
-    }
-
-
-    private interface ProfileQuery {
-        String[] PROJECTION = {
-                ContactsContract.CommonDataKinds.Email.ADDRESS,
-                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
-        };
-
-        int ADDRESS = 0;
-        int IS_PRIMARY = 1;
-    }
-*/
     /**
      * Represents an asynchronous login/registration task used to authenticate
      * the user.
@@ -320,45 +219,25 @@ public class LoginActivity extends AppCompatActivity  {
         @SuppressLint("StringFormatInvalid")
         @Override
         protected Boolean doInBackground(Void... params) {
-            mCursor=getContentResolver().query(AppContract.Manager.MANAGER_URI,null,null,null,null);
-            mCursor.moveToLast();
-            for(int i=mCursor.getCount();i>0;i--,mCursor.moveToPrevious())
-            {
-
-                String cEmail=mCursor.getString(mCursor.getColumnIndexOrThrow(AppContract.Manager.EMAIL_ADDR));
-                String sPassword= null;
+            List<Client> clients = DBManagerFactory.getManager().getClients();
+            String sPassword = "";
+            for (Client client : clients) {
                 try {
-                    sPassword = (SHA_256_Helper.getHash256String(mPassword,mCursor.getLong(mCursor.getColumnIndexOrThrow(AppContract.Manager.SALT))));
+                    sPassword = SHA_256_Helper.getHash256String(mPassword, client.getSalt());
                 } catch (Exception e) {
                     e.printStackTrace();
                     return false;
+                }
+                if (client.getEmailAdrs().contentEquals(mEmail) && client.getPassword().contentEquals(sPassword)) {
+                    mClient = client;
+                    return true;
 
                 }
-                String cPassword =mCursor.getString(mCursor.getColumnIndexOrThrow(AppContract.Manager.PASSWORD));
-
-                    if(mEmail.contentEquals(cEmail) && sPassword.contentEquals(cPassword))
-                        return true;
 
 
             }
             return false;
 
-           /* try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }*/
-
-            // TODO: register the new account here.return true;
         }
 
         @Override
@@ -368,8 +247,9 @@ public class LoginActivity extends AppCompatActivity  {
             SharedPreferences.Editor editor;
             if (success) {
                 editor=mSharedPreferences.edit();
-                editor.putLong(getString(R.string.login_user_id),mCursor.getLong(mCursor.getColumnIndexOrThrow(AppContract.Manager.ID)));
-                editor.putString(getString(R.string.login_user_email),mCursor.getString(mCursor.getColumnIndexOrThrow(AppContract.Manager.EMAIL_ADDR)));
+                editor.putLong(getString(R.string.login_user_id),mClient.getId());
+                editor.putString(getString(R.string.login_user_email),mClient.getEmailAdrs());
+                editor.putString(getString(R.string.login_user_name),mClient.getFirstName()+" "+ mClient.getLastName());
                 editor.putBoolean(getString(R.string.is_login),true);
                 editor.commit();
                 Intent mainIntent =new Intent(LoginActivity.this,MainActivity.class);
@@ -388,28 +268,6 @@ public class LoginActivity extends AppCompatActivity  {
             mAuthTask = null;
             showProgress(false);
         }
-    }
-    private void getManagersCursor() {
-        new AsyncTask<Void, Void, Cursor>() {
-            @Override
-            protected Cursor doInBackground(Void... params) {
-                Cursor cursor = getContentResolver().query(AppContract.Manager.MANAGER_URI, null, null, null, null, null);
-
-                return cursor;
-            }
-
-            @Override
-            protected void onPostExecute(Cursor cursor) {
-                super.onPostExecute(cursor);
-
-                mAuthTask = new UserLoginTask("", "");
-                mCursor =cursor;
-                if (mAuthTask != null) {
-                    mAuthTask = null;
-                }
-
-            }
-        }.execute();
     }
 }
 
